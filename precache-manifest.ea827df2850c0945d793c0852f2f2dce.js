ddca874dbbbf389c7ad00fe00a617d4bf62e7a13
Nicholas Major <nick@nmajor.com>
2020-03-22 12:39:04 +0000

    Updates

---
+++ precache-manifest.ea827df2850c0945d793c0852f2f2dce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71209c00a01288f93066ec4e023d6b9d",
+    "url": "/smart-price-demo/index.html"
+  },
+  {
+    "revision": "8df01f991e3cac7c9054",
+    "url": "/smart-price-demo/static/css/main.fec60048.chunk.css"
+  },
+  {
+    "revision": "e49dec9640333fc0e8bb",
+    "url": "/smart-price-demo/static/js/2.03f197aa.chunk.js"
+  },
+  {
+    "revision": "e8b0ae1bd2f59a85f300b3c60c50941a",
+    "url": "/smart-price-demo/static/js/2.03f197aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8df01f991e3cac7c9054",
+    "url": "/smart-price-demo/static/js/main.5f10547c.chunk.js"
+  },
+  {
+    "revision": "45f1771d0e6db8c14925",
+    "url": "/smart-price-demo/static/js/runtime-main.62f9b873.js"
+  }
+]);
